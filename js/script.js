999b249e735596e8d7e613ba376af6e20eb0817b
Argho Maitra <207898299+oghrmatir@users.noreply.github.com>
2025-04-28 17:03:01 +0530

    Create a webpage with a 16x16 grid of square divs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("Etch-A-Sketch");
+const container = document.querySelector(".container");
+container.style.border = "1px solid black";
+container.style.width = "600px";
+container.style.height = "600px";
+container.style.margin = "0 auto";
+container.style.display = "flex";
+container.style.flexDirection = "column";
+
+for (let i = 0; i < 16; i++) {
+  const outer = document.createElement("div");
+  outer.style.border = "1px solid red";
+  outer.style.display = "flex";
+  outer.style.flex = "1";
+
+  for (let j = 0; j < 16; j++) {
+    const inner = document.createElement("div");
+    inner.style.border = "1px solid yellow";
+    inner.style.flex = "1";
+
+    outer.appendChild(inner);
+  }
+
+  container.appendChild(outer);
+}
